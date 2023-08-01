f615b25a23c144c0119d6a93372658a241aa028a
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-08-01 15:04:23 +0530

    HPE - Task 4 Submission

--- initial/src/test/java/com/example/restservice/RestServiceApplicationTests.java
+++ initial/src/test/java/com/example/restservice/RestServiceApplicationTests.java
@@ -1,14 +1,91 @@
 package com.example.restservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Arrays;
+import java.util.List;
 
-@SpringBootTest
-public class RestServiceApplicationTests {
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(EmployeeController.class)
+class RestServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private EmployeeManager employeeManager;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private final String uri = "http://localhost:8080/employees";
 
 	@Test
-	public void contextLoads() {
+	void canGetAllEmployees() throws Exception {
+
+		Employee employee1 = new Employee(
+				1L,
+				"Robert",
+				"Downey Jr",
+				"robertdowneyjr@hpe.com",
+				"Software Engineer"
+		);
+		Employee employee2 = new Employee(
+				2L,
+				"Emily",
+				"Blunt",
+				"emilyblunt@hpe.com",
+				"Software Engineer"
+		);
+
+		List<Employee> employeeList = Arrays.asList(employee1, employee2);
+
+		Mockito.when(employeeManager.getEmployeeData()).thenReturn(employeeList);
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(uri);
+
+		mockMvc.perform(requestBuilder)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(2)))
+				.andExpect(jsonPath("$[0].email", is("robertdowneyjr@hpe.com")))
+				.andExpect(jsonPath("$[1].firstName", is("Emily")));
+
+	}
+
+	@Test
+	void canAddEmployee() throws Exception {
+
+		Employee robert = new Employee(
+				1L,
+				"Robert",
+				"Downey Jr",
+				"robertdowneyjr@hpe.com",
+				"Software Engineer"
+		);
+
+		Mockito.when(employeeManager.addEmployee(robert)).thenReturn("success");
+
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post(uri)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(robert));
+
+		mockMvc.perform(requestBuilder)
+				.andExpect(status().isOk());
+
 	}
 
 }
